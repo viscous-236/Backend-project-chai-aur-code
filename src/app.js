a4832810c3d9b7c49e28a71d630348adf62a82ea
Vaibhav <vaibhavgoyal2310066@gmail.com>
2025-02-02 12:16:02 +0530

    api testing

--- src/app.js
+++ src/app.js
@@ -14,8 +14,20 @@ app.use(urlencoded({ extended: true , limit: '20kb'})) // limit the body size of
 app.use(express.static('public'))
 app.use(cookieParser()) 
 
-export { app };
+// import routes
+import userRouter from "./routes/user.routes.js";
 
+
+// routes declaration
+app.use("/api/v1/users",userRouter);
+// https://localhost:3000/api/v1/users/register
+// /users act as prefix for all the routes in userRouter or we can say it is base url for all the routes in userRouter
+// when this command is executed the control is transferred to userRouter 
+// all the methods will now be in userRouter
+
+
+
+export { app };
 /*
 in the syntax app.get('/', (req, res) => {
     res.send('Hello World')
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -6,7 +6,7 @@ const asyncHandler = (requestHandler) => {
     }
 }
 
-export default asyncHandler
+export {asyncHandler}
 
 
 /* 
